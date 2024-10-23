55908939530475f8bb776042ab35dd9f3194f298
Brennan Aldrich <brennaldrich@gmail.com>
2024-10-22 20:42:43 -0400

    Created 'Human' Abstract Class in Framework

---
+++ Game/Framework/Human.h
@@ -0,0 +1,48 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+#include "Types.h"
+
+// Responsibilities
+// 1. Know/Return Base HP, MP, DEF, and ATK
+// 2. Declare functions that all classes share
+
+// Human Abstract Class
+class Human {
+public:
+
+    Human(HP Health, MP Magic, DEF Defense, ATK Attack) 
+    : BaseHealth(Health), BaseMagic(Magic), BaseDefense(Defense), BaseAttack(Attack) {}
+
+    virtual ~Human() {}
+
+    virtual void performAttack () = 0;
+    virtual void performDefend () = 0;
+
+    HP getBaseHealth () const { return BaseHealth; }
+    HP getCurrentHealth ()  { return CurrentHealth; }
+    MP getBaseMagic () const { return BaseMagic; }
+    MP getCurrentMagic ()  { return CurrentMagic; }
+    DEF getBaseDefense () const { return BaseDefense; }
+    DEF getCurrentDefense ()  { return CurrentDefense; }
+    ATK getBaseAttack () const { return BaseAttack; }
+    ATK getCurrentAttack () { return CurrentAttack; }
+
+protected:
+
+    HP BaseHealth;
+    HP CurrentHealth;
+    MP BaseMagic;
+    MP CurrentMagic;
+    DEF BaseDefense;
+    DEF CurrentDefense;
+    ATK BaseAttack;
+    ATK CurrentAttack;
+
+
+};
+
+
+
+
+
+#endif
---
+++ Game/Framework/Types/Types.h
@@ -0,0 +1,6 @@
+#include <cstdint>
+
+typedef std::uint8_t HP;
+typedef std::uint8_t MP;
+typedef std::uint8_t ATK;
+typedef std::uint8_t DEF;
